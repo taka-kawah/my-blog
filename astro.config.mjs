466ee9841657c0830a7c234d1ef501cf41622311
河原 <106893036+taka-kawah@users.noreply.github.com>
2025-09-23 02:18:20 +0900

    init

---
+++ astro.config.mjs
@@ -0,0 +1,7 @@
+import { defineConfig } from "astro/config";
+
+// https://astro.build/config
+/** @type {import("astro").AstroUserConfig} */
+const config = defineConfig({})
+
+export default config
