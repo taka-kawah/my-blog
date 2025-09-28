ce1765961de5ddf0efb95ac2d8ad77e28f91d36e
河原 <106893036+taka-kawah@users.noreply.github.com>
2025-09-29 00:19:09 +0900

    リンクアイコン変更

---
+++ src/util/data.ts
@@ -0,0 +1,18 @@
+type accountInfo = {
+  alt: string;
+  href: string;
+  pic: string;
+};
+
+export const accounts: accountInfo[] = [
+  {
+    alt: "Github",
+    href: "https://github.com/taka-kawah",
+    pic: "/mark-github-24.svg",
+  },
+  {
+    alt: "Bluesky",
+    href: "https://bsky.app/profile/ataroh.bsky.social",
+    pic: "/Bluesky_Logo.svg",
+  },
+];
