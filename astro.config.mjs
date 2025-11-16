841057098f6f87697fdc7c49b6755dbf59821c19
河原 <106893036+taka-kawah@users.noreply.github.com>
2025-11-16 18:05:31 +0900

    imagecardを追加

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,11 +3,12 @@ import react from "@astrojs/react"
 import tailwind from "@tailwindcss/vite";
 import remarkMath from 'remark-math';
 import rehypeKatex from 'rehype-katex';
+import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
 /** @type {import("astro").AstroUserConfig} */
 const config = defineConfig({
-    integrations: [react()],
+    integrations: [react(), mdx()],
     vite: {
         plugins: [tailwind()]
     },
